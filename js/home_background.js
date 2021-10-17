9b44fb56aaf9cacaab029d3db88785f119f41c8c
Dayana20 <39806222+Dayana20@users.noreply.github.com>
2021-10-16 20:11:00 -0400

    updated some pages and added some background and one animation

--- js/home_background.js
+++ js/home_background.js
@@ -1,6 +1,6 @@
 function random_c(){
     var color = 'pictures/';
-    var letters = ['Background.png','home_background.jpg','Mouchette.jpg'];
+    var letters = ['fly_background.jpg','home_background.jpg','Mouchette.jpg'];
     color+=letters[Math.floor(Math.random()*letters.length)];
     document.body.style.backgroundImage="url("+color+")";
 }
