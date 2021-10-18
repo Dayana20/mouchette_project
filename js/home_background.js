94d3e35c55a2c61ca795ce797e9475cebf6444f2
Dayana20 <39806222+Dayana20@users.noreply.github.com>
2021-10-17 21:59:28 -0400

    added thank you page

--- js/home_background.js
+++ js/home_background.js
@@ -1,6 +1,6 @@
 function random_c(){
     var color = 'pictures/';
-    var letters = ['fly_background.jpg','home_background.jpg','Mouchette.jpg'];
+    var letters = ['fly_background.jpg','home_background.jpg','mouchette_background.jpg'];
     color+=letters[Math.floor(Math.random()*letters.length)];
     document.body.style.backgroundImage="url("+color+")";
 }
